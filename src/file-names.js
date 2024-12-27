727534034a1fee5e295fc9d4866635e15daf936a
liakh83 <lahn4573@gmail.com>
2024-12-27 17:24:19 +0300

    feat: add solutions for functions: renameFiles, getEmailDomain, calculateHanoi

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const result = [];
+  names.forEach((name) => {
+    if (!result.includes(name)) {
+      result.push(name);
+    } else {
+      let count = 1;
+      let newName = `${name}(${count})`;
+      while (result.includes(newName)) {
+        count += 1;
+        newName = `${name}(${count})`;
+      }
+      result.push(newName);
+    }
+  })
+  return result;
 }
 
 module.exports = {
