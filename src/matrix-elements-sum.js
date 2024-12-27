e897acae4e48013e609ad8e44f201ecf32264854
liakh83 <lahn4573@gmail.com>
2024-12-27 23:02:32 +0300

    feat: add solution for functions: isMAC48Address, getMatrixElementsSum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sumMatrix = 0;
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      if (matrix[i][j] === 0) {
+        for ( let n = 0; n < matrix.length; n += 1 ) {
+          matrix[n][j] = 0;
+        }
+      } else {
+        sumMatrix += matrix[i][j]
+      }
+    }
+  }
+  return sumMatrix;
 }
 
 module.exports = {
