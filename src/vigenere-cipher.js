1586f0a5ee301af14d75f546b77a047ae8b9207a
liakh83 <lahn4573@gmail.com>
2025-01-02 21:08:51 +0300

    feat: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,70 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  isLetter(item) {
+    return item.toUpperCase() !== item.toLowerCase();
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+    return this.process(message, key, true);
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error ('Incorrect arguments!');
+    }
+    return this.process(encryptedMessage, key, false);
+  }
+
+  process (input, key, isEncrypt){
+    const codeA = 'A'.charCodeAt(0);
+    let result = '';
+    let keyIndex = 0;
+    const abcCount = 26;
+
+    for (let i = 0; i < input.length; i += 1) {
+      const item = input[i];
+      if (this.isLetter(item)) {
+
+        let itemIndex = item.toUpperCase().charCodeAt() - codeA;
+        let shift = key.toUpperCase().charCodeAt([keyIndex % key.length]) - codeA;
+
+        const newItemCode = isEncrypt
+          ? (codeA + (itemIndex + shift) % abcCount)
+          : (codeA + (itemIndex - shift + abcCount) % abcCount)
+          
+        result += String.fromCharCode(newItemCode)
+        keyIndex += 1;
+      } else {
+        result += item;
+      }
+    }
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
